8ff29f44df28c78222a2e00c67f7c67dd3995d73
ericndungutse <dav.ndungutse@gmail.com>
2022-12-06 21:14:19 +0200

    Added meddia queries for responsiveness

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+console.log("Hello");
+
+const mobileNav = document.querySelector(".mobile-nav");
+const overlay = document.querySelector(".overlay");
+const openMobileNavBtn = document.querySelector(".open-mobile-nav-icon");
+const closeMobileNavBtn = document.querySelector(".close-mobile-nav");
+
+openMobileNavBtn.addEventListener("click", () => {
+  overlay.classList.add("show-overlay");
+  mobileNav.classList.add("show-mobile-nav");
+});
+
+closeMobileNavBtn.addEventListener("click", () => {
+  overlay.classList.remove("show-overlay");
+  mobileNav.classList.remove("show-mobile-nav");
+});
